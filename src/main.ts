e086fca26297c32433ffe9e97bab493a616abbda
Danny <danymassmcf@gmail.com>
2026-05-30 01:57:46 -0400

    chore(bootstrap): update default port
    decision(bootstrap): default to port 4000 and ignore bootstrap promise

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,6 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 }
-bootstrap();
+void bootstrap();
